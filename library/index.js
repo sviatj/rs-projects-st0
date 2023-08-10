cf285e8de7b96061f130f1041f249374f2fce88c
sviatj <asviatsukha@gmail.com>
2023-08-10 21:26:39 +0300

    feat: add menu and adjust spacings

--- library/index.js
+++ library/index.js
@@ -1,7 +1,6 @@
-console.log( `
-1. Вёрстка валидная +10
-2. Вёрстка семантическая +16
-3. Вёрстка соответствует макету +54
-4. Общие требования к верстке +20
-`        
-    )
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("burger").addEventListener("click", function() 
+    {
+      document.querySelector(".header").classList.toggle("open")
+    })
+})
