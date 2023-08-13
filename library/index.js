2abb296d4edf5d6832c12f459e270af81ea4a424
sviatj <asviatsukha@gmail.com>
2023-08-13 16:10:02 +0300

    feat: add js to menu

--- library/index.js
+++ library/index.js
@@ -1,6 +1,22 @@
+const body = document.body;
+const header = document.querySelector('.header');
+
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("burger").addEventListener("click", function() 
+    document.getElementById("burger").addEventListener("click", function(event) 
     {
+      event.isClick = true
       document.querySelector(".header").classList.toggle("open")
     })
 })
+
+document.getElementById("menu").addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+});
+document.getElementById("burger").addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+});
+document.body.addEventListener('click', event => {
+    if (event._isClickWithInMenu) return;
+    // Действие при клике
+    document.querySelector(".header").classList.remove("open")
+});
