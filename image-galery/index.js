aa7c3c57d686eb88388216b52b50260c0c6eeaf8
sviatj <asviatsukha@gmail.com>
2023-09-30 15:01:49 +0300

    feat: minor fixes

--- image-galery/index.js
+++ image-galery/index.js
@@ -3,12 +3,44 @@ const searchContainer = document.querySelector('.search_container');
 const output = document.querySelector('.output');
 const button = document.querySelector('.button');
 
-const url = 'https://type.fit/api/quotes';
+const key = 'Kf8cMcean-HYKe7Ma6_NWnQnuUJrox0EqBDAjfzJN-s';
 
 let searchInput = "";
-let imgPage = 1;
+let imgPage = 0;
 
-async function getData() {
-    searchInput = search_container.value;
-  }
-  getData();
+
+async function getImages() {
+    searchInput = searchContainer.value;
+    const url = `https://api.unsplash.com/search/photos?page=${imgPage}&query=${searchInput}&client_id=${key}&per_page=9`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    showImages(data);
+    }
+
+
+  function showImages(data) {
+    data.results.map((value) => {
+        let imgUrl = value.urls.regular;
+        const img = document.createElement('img');
+        img.src = imgUrl;
+        img.alt = `image`;
+        output.append(img);
+  })
+}
+
+
+  search.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if(imgPage === 1 || imgPage > 0) {
+        output.innerHTML = '';
+    }
+    getImages();
+  })
+
+  button.addEventListener('click', () => {
+    imgPage++;
+
+    getImages(); 
+  })
