5eb6119a8c6110b4d21b393e98439fd2e4dc2eec
sviatj <asviatsukha@gmail.com>
2023-10-15 17:30:20 +0300

    add win/lose functions

--- random-game/index.js
+++ random-game/index.js
@@ -6,37 +6,61 @@ let startBtn = document.querySelector('.start');
 let startTitle = document.querySelector('.start-title');
 let restartBtn = document.querySelector('.restart');
 let restartTitle = document.querySelector('.restart-title');
+let overlay = document.querySelector('.overlay');
+let scoreItem = document.querySelector('.score-item');
+
+
 
 let changedCard = false;
-let elementCliked = true;
-let cardOne;
-let cardTwo;
+let elementCliked = false;
+let cardOne, cardTwo;
 let flip = 200;
+let time = 30;
+let timeToShowCards = 10000;
 
 
+let restartTimer = setInterval(timerUpdate, 1000);
 
-startBtn.addEventListener('click', function() {
-    startBtn.style.display = 'none';
-    restartBtn.style.display = 'block';
-    restartTitle.style.display = 'block';
-    let time = 30;
-    let restartTimer = setInterval(timerUpdate, 1000);
-    function timerUpdate() {
-        let seconds = time;
+
+(function sort() {
+    cards.forEach(card => {
+        let random = Math.floor(Math.random() * 16)
+        card.style.order = random;
+    })
+})();
+
+
+overlay.addEventListener('click', function() {
+    overlay.style.display = 'none';
+
+ 
+    cards.forEach(card => {
+        card.classList.add('change')
+        setTimeout(() => {
+            card.classList.remove('change')
+        }, timeToShowCards);
+    });
     
-        timer.innerHTML = `Time left: ${seconds}`;
-        time--;
-    
-        if (time < 0) {
-            clearInterval(restartTimer);
-            gameOver();
-        }
-    }
-    return
 })
 
 
+function timerUpdate() {
+    let seconds = time;
+    timer.innerHTML = `Time left: ${seconds}`;
+    time--;
+
+    if (time < 0) {
+        clearInterval(restartTimer);
+        gameOver();
+        youWin();
+    }
+}
+
 function changeCard() {
+    if (elementCliked)
+    return;
+    if (this === cardOne)
+    return;
     this.classList.add('change');
 
     if (changedCard) {
@@ -51,12 +75,12 @@ function changeCard() {
     }
 
     findPair();
-    startTitle.innerHTML = `Click to restart`;
 }
 
 function countFlips() {
     flip = flip + 100;
     score.innerHTML = `Score: ${flip}`;
+    scoreItem.innerHTML = `Score: ${flip}`;
 }
 
 cards.forEach(card => card.addEventListener('click', changeCard))
@@ -73,9 +97,13 @@ function findPair () {
 
 
 function flipBack() {
+elementCliked = true;
+
     setTimeout(() => {
+        elementCliked = false;
         cardOne.classList.remove('change');
         cardTwo.classList.remove('change');
+        elementCliked = false;
     }, 800);
 }
 
@@ -85,16 +113,41 @@ restartBtn.addEventListener('click', function() {
 })
 
 function gameOver() {
-    cards.forEach(card => card.classList.remove('change'))
+    cards.forEach(card => card.classList.remove('change'));
+    clearInterval(restartTimer);
+    overlay.style.display = 'flex';
+    overlay.innerHTML = `Game Over`;
+    location.reload();
+  
 }
 
+function youWin() {
+    if (cardOne.dataset.id === cardTwo.dataset.id) {
+        clearInterval(restartTimer);
+        cards.forEach(card => card.classList.remove('change'));
+        overlay.style.display = 'flex';
+        overlay.innerHTML = `You win`;
+        setTimeout(() => {
+            location.reload()
+        }, 10000);
+    }
+    return
+}
+
+
+function restart() {
+    changedCard = false;
+    changedCard = false;
+    cardOne = null;
+    cardTwo = null;
+}
 
 function lockCards() {
     cardOne.removeEventListener('click', changeCard);
     cardTwo.removeEventListener('click', changeCard);
+
+    restart();
 }
-
-
 
 
 
